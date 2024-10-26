16e24600632ec65202b7d7dd684f387e4a0b5993
shaneching <2536606577@qq.com>
2024-10-26 19:22:10 +0800

    proj0

--- proj0/src/game2048logic/Model.java
+++ proj0/src/game2048logic/Model.java
@@ -7,13 +7,19 @@ import game2048rendering.Tile;
 import java.util.Formatter;
 
 
-/** The state of a game of 2048.
- *  @author P. N. Hilfinger + Josh Hug
+/**
+ * The state of a game of 2048.
+ *
+ * @author P. N. Hilfinger + Josh Hug
  */
 public class Model {
-    /** Current contents of the board. */
+    /**
+     * Current contents of the board.
+     */
     private final Board board;
-    /** Current score. */
+    /**
+     * Current score.
+     */
     private int score;
 
     /* Coordinate System: column x, row y of the board (where x = 0,
@@ -21,71 +27,99 @@ public class Model {
      * to board.tile(x, y).  Be careful!
      */
 
-    /** Largest piece value. */
+    /**
+     * Largest piece value.
+     */
     public static final int MAX_PIECE = 2048;
 
-    /** A new 2048 game on a board of size SIZE with no pieces
-     *  and score 0. */
+    /**
+     * A new 2048 game on a board of size SIZE with no pieces
+     * and score 0.
+     */
     public Model(int size) {
         board = new Board(size);
         score = 0;
     }
 
-    /** A new 2048 game where RAWVALUES contain the values of the tiles
+    /**
+     * A new 2048 game where RAWVALUES contain the values of the tiles
      * (0 if null). VALUES is indexed by (x, y) with (0, 0) corresponding
-     * to the bottom-left corner. Used for testing purposes. */
+     * to the bottom-left corner. Used for testing purposes.
+     */
     public Model(int[][] rawValues, int score) {
         board = new Board(rawValues);
         this.score = score;
     }
 
-    /** Return the current Tile at (x, y), where 0 <= x < size(),
-     *  0 <= y < size(). Returns null if there is no tile there.
-     *  Used for testing. */
+    /**
+     * Return the current Tile at (x, y), where 0 <= x < size(),
+     * 0 <= y < size(). Returns null if there is no tile there.
+     * Used for testing.
+     */
     public Tile tile(int x, int y) {
         return board.tile(x, y);
     }
 
-    /** Return the number of squares on one side of the board. */
+    /**
+     * Return the number of squares on one side of the board.
+     */
     public int size() {
         return board.size();
     }
 
-    /** Return the current score. */
+    /**
+     * Return the current score.
+     */
     public int score() {
         return score;
     }
 
 
-    /** Clear the board to empty and reset the score. */
+    /**
+     * Clear the board to empty and reset the score.
+     */
     public void clear() {
         score = 0;
         board.clear();
     }
 
 
-    /** Add TILE to the board. There must be no Tile currently at the
-     *  same position. */
+    /**
+     * Add TILE to the board. There must be no Tile currently at the
+     * same position.
+     */
     public void addTile(Tile tile) {
         board.addTile(tile);
     }
 
-    /** Return true iff the game is over (there are no moves, or
-     *  there is a tile with value 2048 on the board). */
+    /**
+     * Return true iff the game is over (there are no moves, or
+     * there is a tile with value 2048 on the board).
+     */
     public boolean gameOver() {
         return maxTileExists() || !atLeastOneMoveExists();
     }
 
-    /** Returns this Model's board. */
+    /**
+     * Returns this Model's board.
+     */
     public Board getBoard() {
         return board;
     }
 
-    /** Returns true if at least one space on the board is empty.
-     *  Empty spaces are stored as null.
-     * */
+    /**
+     * Returns true if at least one space on the board is empty.
+     * Empty spaces are stored as null.
+     */
     public boolean emptySpaceExists() {
         // TODO: Task 1. Fill in this function.
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                if (board.tile(i, j) == null) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -96,6 +130,14 @@ public class Model {
      */
     public boolean maxTileExists() {
         // TODO: Task 2. Fill in this function.
+        int count = 0;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                if (board.tile(i, j) != null && board.tile(i, j).value() == MAX_PIECE) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -107,45 +149,93 @@ public class Model {
      */
     public boolean atLeastOneMoveExists() {
         // TODO: Task 3. Fill in this function.
+        if (emptySpaceExists()) {
+            return true;
+        }
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board.size(); j++) {
+                if (i + 1 < board.size() && board.tile(i, j).value() == board.tile(i + 1, j).value()) {
+                    return true;
+                }
+                if (j + 1 < board.size() && board.tile(i, j).value() == board.tile(i, j + 1).value()) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     /**
      * Moves the tile at position (x, y) as far up as possible.
-     *
+     * <p>
      * Rules for Tilt:
      * 1. If two Tiles are adjacent in the direction of motion (ignoring empty space)
-     *    and have the same value, they are merged into one Tile of twice the original
-     *    value and that new value is added to the score instance variable
+     * and have the same value, they are merged into one Tile of twice the original
+     * value and that new value is added to the score instance variable
      * 2. A tile that is the result of a merge will not merge again on that
-     *    tilt. So each move, every tile will only ever be part of at most one
-     *    merge (perhaps zero).
+     * tilt. So each move, every tile will only ever be part of at most one
+     * merge (perhaps zero).
      * 3. When three adjacent tiles in the direction of motion have the same
-     *    value, then the leading two tiles in the direction of motion merge,
-     *    and the trailing tile does not.
+     * value, then the leading two tiles in the direction of motion merge,
+     * and the trailing tile does not.
      */
     public void moveTileUpAsFarAsPossible(int x, int y) {
         Tile currTile = board.tile(x, y);
+        if (currTile == null) {
+            return;
+        }
         int myValue = currTile.value();
         int targetY = y;
-
         // TODO: Tasks 5, 6, and 10. Fill in this function.
+        while (targetY < board.size() - 1 && board.tile(x, targetY + 1) == null) {
+            targetY++;
+        }
+        if (targetY == board.size() - 1) {
+            board.move(x, targetY, currTile);
+        } else {
+            Tile nextTile = board.tile(x, targetY + 1);
+            if (nextTile.value() == myValue && !nextTile.wasMerged()) {
+                board.move(x, targetY + 1, currTile);
+                score += 2 * myValue;
+            }
+            if (nextTile.value() == myValue && nextTile.wasMerged()) {
+                board.move(x, targetY, currTile);
+            }
+            if (nextTile.value() != myValue) {
+                board.move(x, targetY, currTile);
+            }
+        }
     }
 
-    /** Handles the movements of the tilt in column x of board B
+    /**
+     * Handles the movements of the tilt in column x of board B
      * by moving every tile in the column as far up as possible.
      * The viewing perspective has already been set,
      * so we are tilting the tiles in this column up.
-     * */
+     */
     public void tiltColumn(int x) {
         // TODO: Task 7. Fill in this function.
+        for (int y = board.size() - 2; y >= 0; y--) {
+            moveTileUpAsFarAsPossible(x, y);
+        }
+
     }
 
     public void tilt(Side side) {
         // TODO: Tasks 8 and 9. Fill in this function.
+        board.setViewingPerspective(side);
+        for (int x = 0; x < board.size(); x++) {
+            for (int y = board.size() - 2; y >= 0; y--) {
+                moveTileUpAsFarAsPossible(x, y);
+            }
+        }
+        board.setViewingPerspective(Side.NORTH);
+
     }
 
-    /** Tilts every column of the board toward SIDE.
+    /**
+     * Tilts every column of the board toward SIDE.
      */
     public void tiltWrapper(Side side) {
         board.resetMerged();
